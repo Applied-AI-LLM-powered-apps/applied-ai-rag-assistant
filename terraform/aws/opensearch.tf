9ad62ae66ecfc40754e6255dbfcb41e5ebec0852
alexgoyer1 <agoyer@finaxys.com>
2024-07-10 15:54:38 +0200

    Update opensearch to be VPC accessible and return to chroma in config.ini

--- terraform/aws/opensearch.tf
+++ terraform/aws/opensearch.tf
@@ -10,21 +10,6 @@ data "aws_iam_policy_document" "opensearch_domain_policy" {
     actions   = ["es:*"]
     resources = ["arn:aws:es:eu-west-1:441525731509:domain/ai-assistant/*"]
   }
-  statement {
-    principals {
-      type        = "AWS"
-      identifiers = var.opensearch_allowed_users_and_policy_arn #list of arn
-    }
-
-    actions   = ["es:*"]
-    resources = ["arn:aws:es:eu-west-1:441525731509:domain/ai-assistant/*"]
-
-    condition {
-      test     = "IpAddress"
-      variable = "aws:SourceIp"
-      values   = ["172.16.1.0/8"]
-    }
-  }
 }
 
 resource "aws_opensearch_domain" "ai_assistant_opensearch_domain" {
@@ -37,16 +22,12 @@ resource "aws_opensearch_domain" "ai_assistant_opensearch_domain" {
     instance_count = 1
     multi_az_with_standby_enabled = false
   }
-
-  advanced_security_options {
-    enabled                        = true
-    anonymous_auth_enabled         = false
-    internal_user_database_enabled = true
-    master_user_options {
-      master_user_name     = var.opensearch_dashboard_user
-      master_user_password = var.opensearch_dashboard_password
-    }
+  
+  vpc_options {
+    subnet_ids         = [data.aws_subnet.ai_assistant_subnet_1.id]
+    security_group_ids = [aws_security_group.ai_assistant_security_group.id]
   }
+
   domain_endpoint_options {
     enforce_https       = true
     tls_security_policy = "Policy-Min-TLS-1-2-2019-07"
